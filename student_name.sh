95b39684074bc394df72d923ecabd7cc3068e7c5
ajinkyamhasrup <ajinkya.mhasrup@gmail.com>
2025-02-11 14:52:34 +0530

    Update student_name.sh

--- student_name.sh
+++ student_name.sh
@@ -1,21 +1,12 @@
-pipeline {
-    agent any
-    stages {
-        stage('Run Script') {
-            steps {
-                sh '''
-                if [ $# -ne 2 ]; then
-                    echo "Usage: $0 <student_name> <city>"
-                    exit 1
-                fi
+#!/bin/bash
 
-                STUDENT_NAME=$1
-                CITY=$2
+if  [ $# -ne 2 ]; then
+	echo "Usage: $0 <student_name> <city>"
+	exit 1
+fi
 
-                echo "Student Name: $STUDENT_NAME"
-                echo "CITY: $CITY"
-                '''
-            }
-        }
-    }
-}
+STUDENT_NAME=$1
+CITY=$2
+
+echo "Student Name: $STUDENT_NAME"
+echo "CITY: $CITY"

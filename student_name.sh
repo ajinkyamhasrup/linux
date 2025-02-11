9544207133ba1070ce8d6635d22e35e7e6a134f8
ajinkyamhasrup <ajinkya.mhasrup@gmail.com>
2025-02-11 14:58:23 +0530

    Update student_name.sh

--- student_name.sh
+++ student_name.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
-# Prompt the user for input if parameters are not provided
 if [ -z "$1" ]; then
     read -p "Enter Student Name: " STUDENT_NAME
 else
     STUDENT_NAME=$1
 fi
-
 if [ -z "$2" ]; then
     read -p "Enter City: " CITY
 else
     CITY=$2
 fi
 
-# Display the input values
 echo "Student Name: $STUDENT_NAME"
 echo "City: $CITY"

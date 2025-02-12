c84d14891e2a0240b59562a90184133055215d3b
ajinkyamhasrup <ajinkya.mhasrup@gmail.com>
2025-02-12 20:32:05 +0530

    Update student_name.sh

--- student_name.sh
+++ student_name.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <student_name> <city>"
-    exit 1
-fi
+# #!/bin/bash
+# if [ $# -ne 2 ]; then
+#     echo "Usage: $0 <student_name> <city>"
+#     exit 1
+# fi
 STUDENT_NAME=$1
 CITY=$2
 echo "Student Name: $STUDENT_NAME"
